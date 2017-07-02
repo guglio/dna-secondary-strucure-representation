e143984b14c4a2d59bc88c36516e7ae873221b63
Guglielmo Turco <guglielmo.turco@gmail.com>
2017-07-02 13:08:59 +0200

    Navigartion header

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import dna from './icons/dna.gif';
+import { Navbar } from 'react-bootstrap';
+import {Link} from 'react-router-dom'
+function Header(){
+  return (
+    <Navbar className="navbar-dna">
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/"><img src={dna} className="brandImg" alt="DNA Icon"/>DNA Secondary Structure Representation</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+    </Navbar>
+  );
+}
+
+export default Header;
