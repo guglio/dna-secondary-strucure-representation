ae39d0231683e3b2c63b3a1e9b4443637b4e7165
Guglielmo Turco <guglielmo.turco@gmail.com>
2017-07-07 00:01:46 +0200

    Add text to landing page, inputs placeholder and initial validation of data (lenght and regex)

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,8 @@ class Home extends Component {
     super(props);
     this.state = {
       dna:'',
-      dbn:''
+      dbn:'',
+      warning:''
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,8 +24,26 @@ class Home extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const path = '/'+this.state.dna+'/'+this.state.dbn;
-    this.props.history.push(path);
+    let dna = this.state.dna;
+    let dbn = this.state.dbn;
+    let errState = '';
+
+    if(dna.length <= 0)
+      errState += 'DNA sequence must be > 0 ';
+    if(dbn.length <= 0)
+      errState += "DBN sequence must be > 0 ";
+    if(!dna.match(/^[ACTGactg]+$/))
+      errState += "DNA sequence not accepted (only C,G,A,T base) ";
+    if(!dbn.match(/^[.()]+$/))
+      errState += "DBN notation not accepted (only dots and parentheses) ";
+    if(errState === ''){
+      let path = this.state.dna+"/"+this.state.dbn;
+      this.props.history.push(path);
+    }
+    else{
+      this.setState({warning:errState});
+    }
+
   }
 
   render() {
@@ -32,13 +51,16 @@ class Home extends Component {
       <Grid>
         <Row>
           <Col md={12}>
+            <p>This tool create an interactive 2D representation of a molecule, given its DNA sequence and 2D structure in dot-bracket notation (DBN).</p>
+          </Col>
+          <Col md={12}>
             <Form horizontal id="test" onSubmit={this.handleSubmit}>
               <FormGroup controlId="formHorizontalEmail">
                 <Col  sm={2}>
                   DNA
                 </Col>
                 <Col sm={10}>
-                  <FormControl type="text" name="dna" value={this.state.dna} onChange={this.handleInputChange} />
+                  <FormControl type="text" name="dna" value={this.state.dna} onChange={this.handleInputChange} placeholder="DNA Sequence with standard A, C, T, G bases"/>
                 </Col>
               </FormGroup>
               <FormGroup controlId="formHorizontalPassword">
@@ -46,8 +68,7 @@ class Home extends Component {
                   DBN
                 </Col>
                 <Col sm={10}>
-                  <FormControl type="text" name="dbn" value={this.state.dbn} onChange={this.handleInputChange}
-                  />
+                  <FormControl type="text" name="dbn" value={this.state.dbn} onChange={this.handleInputChange} placeholder="2D notation with only dots and parentheses (e.g. no pseudo knots)"/>
                 </Col>
               </FormGroup>
 
@@ -62,8 +83,7 @@ class Home extends Component {
           </Col>
         </Row>
         <Row>
-          <code>{this.state.dna}</code><br />
-          <code>{this.state.dbn}</code>
+          <p className="bg-danger">{this.state.warning}</p>
         </Row>
       </Grid>
     );
